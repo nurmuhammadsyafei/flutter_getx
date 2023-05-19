000886d1a4547bb3397e3753b32c3d6afd718164
Syafei Lenovo <nurmuhammadsyafei1447@gmail.com>
2023-05-19 08:39:44 +0700

    penambahan menu ambil data statefull method get ke ki_web_service

--- lib/app/modules/home_stateful/views/home_stateful_view.dart
+++ lib/app/modules/home_stateful/views/home_stateful_view.dart
@@ -13,7 +13,10 @@ class HomeStatefulView extends StatefulWidget {
 }
 
 class _HomeStatefulView extends State<HomeStatefulView> {
+  HttpStateful dataResponse2 = HttpStateful();
+  HttpStateful dataResponse3 = HttpStateful();
   HttpStateful dataResponse = HttpStateful();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +28,7 @@ class _HomeStatefulView extends State<HomeStatefulView> {
           width: double.infinity,
           margin: const EdgeInsets.all(20),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               FittedBox(
                 child: Text(
@@ -75,7 +78,124 @@ class _HomeStatefulView extends State<HomeStatefulView> {
                   child: Text(
                     "POST DATA",
                     style: TextStyle(fontSize: 25),
-                  ))
+                  ),
+                ),
+              OutlinedButton(
+                  onPressed: () {
+                    HttpStateful.konekAPI("2")
+                    .then((value) {
+
+                      // setstate
+                      // print(value);
+                      setState(() {
+                        dataResponse2 = value;
+                      });
+                    }); //.then((value) {
+                    // });
+                  },
+                  child: Text(
+                    "GET DATA",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+
+                // SHOWAN = -=-=-=-==-=--=-=--=-=-=-==-=--=-=--=-=-=-==-=--=-=--=-=-=-==-=--=-=--=-=-=-==-=--=-=--=-=-=-==-=--=-=-
+                FittedBox(
+                child: Text(
+                  (dataResponse2.email == 0)
+                      ? "NPP : Belum ada data"
+                      : "NPP : ${dataResponse2.email}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 20),
+              FittedBox(
+                child: Text(
+                  (dataResponse2.fullname == 0)
+                      ? "NAMA : Belum ada data"
+                      : "NAMA : ${dataResponse2.fullname}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 20),
+              FittedBox(
+                child: Text(
+                  (dataResponse2.aidi == 0)
+                      ? "SPV : Belum ada data"
+                      : "SPV : ${dataResponse2.aidi}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              OutlinedButton(
+                  onPressed: () {
+                    HttpStateful.oracleAPI()
+                    .then((value) {
+                      setState(() {
+                        dataResponse3=value;
+                      });
+                    });
+                  },
+                  child: Text(
+                    "GET ORACLE DATA",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+              SizedBox(height: 20),
+              FittedBox(
+                child: Text(
+                  (dataResponse3.id_karyawan == "")
+                      ? "Id_karyawan : Belum ada data"
+                      : "Id_karyawan : ${dataResponse3.id_karyawan}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 20),
+              FittedBox(
+                child: Text(
+                  (dataResponse3.nama == "")
+                      ? "NAMA : Belum ada data"
+                      : "NAMA : ${dataResponse3.nama}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 20),
+              FittedBox(
+                child: Text(
+                  (dataResponse3.alamat == "")
+                      ? "ALAMAT : Belum ada data"
+                      : "ALAMAT : ${dataResponse3.alamat}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 20),
+              FittedBox(
+                child: Text(
+                  (dataResponse3.telp == "")
+                      ? "TELP : Belum ada data"
+                      : "TELP : ${dataResponse3.telp}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 20),
+              FittedBox(
+                child: Text(
+                  (dataResponse3.branch == "")
+                      ? "BRANCH : Belum ada data"
+                      : "BRANCH : ${dataResponse3.branch}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 20),
+              FittedBox(
+                child: Text(
+                  (dataResponse3.spv == "")
+                      ? "SPV : Belum ada data"
+                      : "SPV : ${dataResponse3.spv}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              
+                // SHOWAN-=-=-=-==-=--=-=--=-=-=-==-=--=-=--=-=-=-==-=--=-=--=-=-=-==-=--=-=--=-=-=-==-=--=-=--=-=-=-==-=--=-=-
             ],
           ),
         ));
--- lib/models/http_stateful.dart
+++ lib/models/http_stateful.dart
@@ -1,12 +1,17 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:http/http.dart' as http;
 
 class HttpStateful {
-  String id, name, job, createdAt;
+  String id, name, job, createdAt,fullname, email, avatar,aidi,id_karyawan,nama,telp,alamat,branch,spv;
+  // var id_karyawan,spv; 
 
   HttpStateful(
-      {this.createdAt = "", this.job = "", this.name = "", this.id = ""});
+      {this.createdAt = "", this.job = "", this.name = "", this.id = "",
+      this.avatar="", this.email="", this.fullname="",this.aidi=""
+      ,this.id_karyawan="",this.telp="",this.alamat="",this.branch="",this.spv="",this.nama = ""
+      });
 
   static Future<HttpStateful> connectAPI(String name, String job) async {
     Uri url = Uri.parse("https://reqres.in/api/users");
@@ -17,6 +22,41 @@ class HttpStateful {
       name: data["name"],
       job: data["job"],
       createdAt: data["createdAt"],
+    );
+  }
+
+
+  static Future<HttpStateful> oracleAPI()async {
+    // Uri url = Uri.parse("http://192.168.1.12:3003/example/db");
+    Uri url = Uri.parse("http://10.146.49.160:3003/example/db");
+    var hasilResponse = await http.get(url);
+    var data = (json.decode(hasilResponse.body))["data"];
+    print(data);
+
+    return HttpStateful(
+        id_karyawan:data[0]["ID_KARYAWAN"],
+        nama: data[0]["NAMA"],
+        spv: data[0]["SPV"],
+        alamat:data[0]["ALAMAT"],
+        branch:data[0]["BRANCH"],
+        telp:data[0]["TELP"],
+        );
+  } 
+  
+  static Future<HttpStateful> konekAPI(String id) async {
+    Uri url = Uri.parse("https://reqres.in/api/users/" + id);
+
+    var hasilResponse = await http.get(url);
+
+    var data = (json.decode(hasilResponse.body))["data"];
+
+    // print(data);
+
+    return HttpStateful(
+      id: data["id"].toString(),
+      fullname: data["first_name"] + " " + data["last_name"],
+      avatar: data["avatar"],
+      email: data["email"],
     );
   }
 }
